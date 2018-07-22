9a549ea9213717ba5f02e352f5417cdc9a9ed144
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-22 15:02:17 -0500

    add redux store and react-redux-firebase boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, compose } from 'redux';
+import { reactReduxFirebase } from 'react-redux-firebase';
+import { reduxFirestore } from 'redux-firestore';
+import rootReducer from './reducers';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+import App from './App';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnp8mX7tmPbnSAwkUfc7iLg0vJ85WcLLY',
+  authDomain: 'plant-diary-1e535.firebaseapp.com',
+  databaseURL: 'https://plant-diary-1e535.firebaseio.com',
+  projectId: 'plant-diary-1e535',
+  storageBucket: 'plant-diary-1e535.appspot.com',
+  messagingSenderId: '512439992397'
+};
+
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase)
+)(createStore);
+
+const initialState = {};
+const store = createStoreWithFirebase(rootReducer, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
