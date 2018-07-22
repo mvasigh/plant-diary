76bce809e1389cf5146bba85b524ea0e663721f8
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-22 15:04:29 -0500

    add basic App routing and scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import ScrollToTop from './components/ScrollToTop';
+import Navbar from './components/Navbar';
+import Home from './views/Home';
+import DayView from './views/DayView';
+import UserLogin from './views/UserLogin';
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <Router>
+        <ScrollToTop>
+          <Navbar />
+          <Switch>
+            <Route path="/day" component={DayView} />
+            <Route path="/users/login" component={UserLogin} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </ScrollToTop>
+      </Router>
+    );
   }
 }
 
