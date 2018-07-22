3084fe58040f9753a6371f8ea1adf6665cbcb3f5
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-22 15:02:47 -0500

    create Navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar is-primary">
+        <div className="navbar-brand" />
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
