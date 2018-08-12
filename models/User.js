df5612bae5e3a8c4da384ede4f6125c3161d7267
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-11 21:14:30 -0500

    save hashed password and add comparePassword model method

--- models/User.js
+++ models/User.js
@@ -1,8 +1,39 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new mongoose.Schema({
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
   password: String
 });
+
+// on save hook, encrypt and save the password
+userSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
