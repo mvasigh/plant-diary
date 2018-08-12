0554c67eac5b2f042e0d4f723f3a56e7606f2ee2
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-11 22:08:47 -0500

    create requireAuth and requireSignIn middleware

---
+++ middleware/index.js
@@ -0,0 +1,4 @@
+const passport = require('passport');
+
+exports.requireAuth = passport.authenticate('jwt', { session: false });
+exports.requireSignIn = passport.authenticate('local', { session: false });
