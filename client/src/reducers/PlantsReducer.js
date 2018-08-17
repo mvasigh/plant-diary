b2e0b0d413d81d0609b120fe8a79d30c898e9b8b
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-16 22:26:03 -0500

    remove old firebase stuff from reducers

---
+++ client/src/reducers/PlantsReducer.js
@@ -0,0 +1,6 @@
+export default function plantsReducer(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { firebaseReducer } from 'react-redux-firebase';
-import { firestoreReducer } from 'redux-firestore';
+import plantsReducer from './PlantsReducer';
 
 const rootReducer = combineReducers({
-  firebase: firebaseReducer,
-  firestore: firestoreReducer
+  plants: plantsReducer
 });
 
 export default rootReducer;
