4e16455850db1f0d0bb1be2f3fed0f79f0f99d33
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-17 20:30:25 -0500

    add GET_PLANT and GET_ALL_PLANTS actions to reducer

--- client/src/reducers/PlantsReducer.js
+++ client/src/reducers/PlantsReducer.js
@@ -1,5 +1,15 @@
+import { GET_PLANT, GET_ALL_PLANTS } from '../actions/Types';
+
 export default function plantsReducer(state = {}, action) {
   switch (action.type) {
+    case GET_ALL_PLANTS:
+      return action.payload.data.response.reduce((acc, plant) => {
+        acc[plant._id] = plant;
+        return acc;
+      });
+    case GET_PLANT:
+      const plant = action.payload.data.response;
+      return { [plant._id]: plant, ...state };
     default:
       return state;
   }
