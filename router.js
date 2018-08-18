87a9b4ce3ef350ad89c8fd5a23bb84c93f80be2c
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-17 21:09:10 -0500

    scaffold play routes

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 const indexRoutes = require('./routes/api/index'),
   usersRoutes = require('./routes/api/users'),
-  plantsRoutes = require('./routes/api/plants');
+  plantsRoutes = require('./routes/api/plants'),
+  dayRoutes = require('./routes/api/day');
 
 module.exports = function(app) {
   app.use('/api', indexRoutes);
   app.use('/api/users', usersRoutes);
   app.use('/api/plants', plantsRoutes);
+  app.use('/api/day', dayRoutes);
 };
