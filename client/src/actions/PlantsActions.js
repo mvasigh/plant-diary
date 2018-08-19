f09721ce94329b2b4090e92beb4347867e3534e6
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-18 20:45:55 -0500

    fix relative path

--- client/src/actions/PlantsActions.js
+++ client/src/actions/PlantsActions.js
@@ -1,5 +1,5 @@
 import { GET_PLANT, GET_ALL_PLANTS } from './Types';
-import { http as axios } from '../../services/axios';
+import { http as axios } from '../services/axios';
 
 export function getPlant(plantId) {
   const request = axios.get(`/api/plants/${plantId}`);
