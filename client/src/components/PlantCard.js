3f30be20d933adc77e4446c457875d27680c6afe
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-18 23:45:51 -0500

    scaffold header and detail bar

--- client/src/components/PlantCard.js
+++ client/src/components/PlantCard.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 
 class PlantCard extends Component {
+  renderHeader() {
+    const { name, type } = this.props;
+    return (
+      <article className="media">
+        <figure className="media-left">
+          <p className="image is-64x64">
+            <img
+              style={{
+                borderRadius: '50%'
+              }}
+              src="https://bulma.io/images/placeholders/128x128.png"
+            />
+          </p>
+        </figure>
+        <div className="media-content">
+          <p className="title is-3">{name}</p>
+          <p className="subtitle is-5">{type}</p>
+        </div>
+      </article>
+    );
+  }
+
+  renderDetailBar() {
+    const { preferences } = this.props;
+    const { waterFrequency, fertilizerFrequency, sunlightAmount } = preferences;
+    return (
+      <div className="level">
+        <div className="level-item">{`water every ${waterFrequency} hrs`}</div>
+        <div className="level-item">{`fert. ${fertilizerFrequency} days`}</div>
+        <div className="level-item">{`${sunlightAmount} sun`}</div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="box">
-        <div className="content">
-          <h1>Hello world</h1>
-        </div>
+        {this.renderHeader()}
+        {this.renderDetailBar()}
       </div>
     );
   }
