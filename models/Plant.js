5a9d0f3a6f871017db96be0947f2381b881241ff
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-18 20:47:04 -0500

    update plant model

--- models/Plant.js
+++ models/Plant.js
@@ -2,6 +2,23 @@ const mongoose = require('mongoose');
 
 const plantSchema = new mongoose.Schema({
   name: String,
+  type: String,
+  imageUrl: String,
+  preferences: {
+    // Hours between watering
+    waterFrequency: Number,
+    // Days between fertilization
+    fertilizerFrequency: Number,
+    sunlightAmount: {
+      type: String,
+      default: 'full'
+    },
+    notes: String
+  },
+  history: {
+    water: [Number],
+    fertilizer: [Number]
+  },
   owners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
