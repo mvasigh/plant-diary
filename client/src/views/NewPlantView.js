a4b5f56e4c8dac66eaf7dc6019b2234c5574ab91
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-21 21:13:51 -0500

    build out part of NewPlantView

--- client/src/views/NewPlantView.js
+++ client/src/views/NewPlantView.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Section from '../components/Section';
+import TextField from '../components/TextField';
 
 class NewPlantView extends Component {
+  renderNewPlantForm() {
+    return (
+      <form>
+        <TextField label="Plant Name" />
+        <TextField label="Type" />
+      </form>
+    );
+  }
+
   render() {
     return (
       <div className="app-page">
-        <section className="section">New Plant View</section>
+        <Section>
+          <div className="columns">
+            <div className="column is-8 is-offset-2">
+              <div className="box">
+                <p className="title is-6 has-text-centered">Add a Plant</p>
+                {this.renderNewPlantForm()}
+              </div>
+            </div>
+          </div>
+        </Section>
       </div>
     );
   }
