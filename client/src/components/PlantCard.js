ddde49c2fea712f1c046a6f71951ddeaa1714fbc
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-25 18:41:44 -0500

    add detail icons

--- client/src/components/PlantCard.js
+++ client/src/components/PlantCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import moment from 'moment';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
@@ -9,16 +10,29 @@ import {
   Avatar,
   Button,
   CardContent,
-  IconButton
+  IconButton,
+  Grid,
+  Typography
 } from '@material-ui/core';
 import GrainIcon from '@material-ui/icons/Grain';
 import WavesIcon from '@material-ui/icons/Waves';
 import EditIcon from '@material-ui/icons/Edit';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
 
 moment.relativeTimeThreshold('m', 60);
 moment.relativeTimeThreshold('h', 24 * 26);
 
 const styles = theme => ({
+  detail: {
+    flexGrow: 1
+  },
+  iconDisplay: {
+    textAlign: 'center'
+  },
+  detailIcon: {
+    fontSize: 24,
+    color: '#ccc'
+  },
   card: {
     marginBottom: '24px'
   },
@@ -31,30 +45,46 @@ const styles = theme => ({
 });
 
 class PlantCard extends Component {
-  renderDetailBar() {
-    const { water, fertilizer } = this.props.plant.history;
-    const waterTags = water.map(entry => (
-      <span key={entry} className="tag is-info">
-        {moment(entry).fromNow()}
-      </span>
-    ));
-    const fertilizerTags = fertilizer.map(entry => (
-      <span key={entry} className="tag is-success">
-        {moment(entry).fromNow()}
-      </span>
-    ));
-    return (
-      <div className="tags">
-        {waterTags}
-        {fertilizerTags}
-      </div>
-    );
-  }
-
   getTimeUntil = (historyArr, numToAdd, unit) => {
     const timeTil = moment(Math.max(...historyArr)).add(numToAdd, unit);
     return timeTil.isBefore(moment()) ? 'now' : timeTil.fromNow(true);
   };
+
+  getMostRecent = historyArr => {
+    return moment(Math.max(...historyArr));
+  };
+
+  renderDetailBar() {
+    const { plant, classes } = this.props;
+    const iconDisplays = [
+      {
+        label: 'Last Watered',
+        icon: <WavesIcon className={classes.detailIcon} />,
+        detail: this.getMostRecent(plant.history.water).fromNow()
+      },
+      {
+        label: 'Last Fertilized',
+        icon: <GrainIcon className={classes.detailIcon} />,
+        detail: this.getMostRecent(plant.history.fertilizer).fromNow()
+      },
+      {
+        label: 'Sunlight',
+        icon: <WbSunnyIcon className={classes.detailIcon} />,
+        detail: _.startCase(plant.preferences.sunlight)
+      }
+    ].map((item, i) => (
+      <Grid className={classes.iconDisplay} item xs={4} key={i}>
+        {item.icon}
+        <Typography>{item.label}</Typography>
+        <Typography variant="caption">{item.detail}</Typography>
+      </Grid>
+    ));
+    return (
+      <Grid container className={classes.detail} spacing={0}>
+        {iconDisplays}
+      </Grid>
+    );
+  }
 
   renderActionBar() {
     const { plant, classes, onWaterClick, onFertilizeClick } = this.props;
@@ -100,7 +130,7 @@ class PlantCard extends Component {
     return (
       <Card className={classes.card}>
         <CardHeader
-          avatar={<Avatar>{plant.name[0]}</Avatar>}
+          avatar={<Avatar>{plant.name[0].toUpperCase()}</Avatar>}
           title={plant.name}
           titleTypographyProps={{ variant: 'headline' }}
           subheader={plant.type}
