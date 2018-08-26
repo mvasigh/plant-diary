83a22ed517f5ef8056ea933a4d19b4213b449e57
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-26 07:56:12 -0500

    add toaster reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import toasterReducer from './ToasterReducer';
 import plantsReducer from './PlantsReducer';
 
 const rootReducer = combineReducers({
+  toaster: toasterReducer,
   plants: plantsReducer
 });
 
